77ed2fc0fabfef3865521276eeb38cd4d6f0cd09
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2023-03-18 15:59:45 +0100

    chore: init

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,40 @@
 import {Web3Button} from "@web3modal/react";
 import {useAccount} from "wagmi";
+import Image from "next/image";
+import {useState, useEffect} from "react";
 
 import {Account} from "../components";
+import DepositModule from "../components/DepositModule";
+import RewardsModule from "../components/RewardsModule";
 
 function Page() {
     const {isConnected} = useAccount();
+    // const [depositComp, setDepositcomp] = useState(true);
 
-    return (
-        <div className="px-4">
-            <div className="flex justify-between border mt-4">
-                <h1>wagmi + Web3Modal + Next.js</h1>
+    useEffect(() => {
+        setTimeout(() => {
+            console.log("Hello, World!");
+        }, 3000);
+    }, []);
+
+    const Header = () => {
+        return (
+            <div className="flex justify-between pt-4 h-14">
+                <Image src="/GDXLogo.png" width="150" height="50" alt={""} />
                 <Web3Button />
             </div>
+        );
+    };
 
-            {isConnected && <Account />}
+    return (
+        <div className="flex-auto px-8 min-h-screen bg-[#1C1E30]">
+            {Header()}
+            <div className="flex justify-center items-center min-h-[700px] flex-col   ">
+                <DepositModule />
+                <RewardsModule />
+            </div>
+
+            {/* {isConnected && <Account />} */}
         </div>
     );
 }
